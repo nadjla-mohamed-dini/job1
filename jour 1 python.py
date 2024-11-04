1c3e436d79a35d3cc723f39e4174778043b8a79d
Nadjla-Mohamed-Dini <nadjla.mohamed-dini@laplateforme.io>
2024-11-04 12:00:15 +0100

    toto

---
+++ jour 1 python.py
@@ -0,0 +1,47 @@
+import string
+alphabet = list(string.ascii_letters)
+print(alphabet)
+# JOB 4
+chaine = alphabet
+chaine_inverse = chaine[::-1]
+print(chaine_inverse)
+# JOB 5
+ma_string = "Je suis une String"
+print(ma_string)
+# JOB 6
+num1 = 40
+num2 = 2 
+print(num1+num2)
+ # JOB 7
+num1 = 3
+num2 = 14
+print(num1*num2)
+# JOB 8
+nom = "cahier"
+prix = 5
+quantité = 10
+print(nom)
+print(prix)
+print(quantité)
+nom1 = "pencil"
+prix1 = 5
+quantité1 = 2
+nouvprix = prix1 + 10/100
+print(nom1)
+print(quantité1)
+print(nouvprix)
+# JOB 9
+montantinitial = 25000
+rend = 35/100
+gain = 25000 * 0.35
+print(gain)
+montantinitial += 5000
+rend += 37/100
+gain = montantinitial * rend
+print(gain)
+montantinitial = 21600
+gain -= 10/100
+rend -= 1/100 
+gain = montantinitial * rend 
+print(gain)
+# JOB 10
